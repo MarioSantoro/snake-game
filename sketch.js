dfe7bc9d734e0c55639938613142f09514703ed5
MarioSantoro <mariosantoro124@gmail.com>
2023-07-18 19:40:55 +0200

    added a new style and added new function for GameOver

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 let snake;
 let scl = 20;
 let food;
+let gameOver = false;
+let gameoverElement = document.querySelector('div.gameover');
+let buttonPlay = document.getElementById('play');
+buttonPlay = document.addEventListener('click', function () {
+    gameoverElement.classList.remove('active');
+    gameOver = false;
+    snake.clear();
+})
+
 function setup() {
     createCanvas(800, 800);
     snake = new Snake();
     frameRate(10)
-
     pickLocation();
 }
 
@@ -17,12 +25,12 @@ function pickLocation() {
 }
 
 function draw() {
-    background(221);
+    background(32, 40, 54);
     snake.update();
     snake.show()
 
     fill(255, 0, 10);
-    rect(food.x, food.y, scl, scl)
+    rect(food.x, food.y, scl, scl, 20)
 
     if (snake.eat(food)) {
         pickLocation();
@@ -30,23 +38,40 @@ function draw() {
 }
 
 function keyPressed() {
-    if (keyCode === UP_ARROW) {
+    if (keyCode === UP_ARROW && snake.yspeed != 1) {
         snake.dir(0, -1);
-    } else if (keyCode === DOWN_ARROW) {
+    } else if (keyCode === DOWN_ARROW && snake.yspeed != -1) {
         snake.dir(0, 1);
-    } else if (keyCode === LEFT_ARROW) {
+    } else if (keyCode === LEFT_ARROW && snake.xspeed != 1) {
         snake.dir(-1, 0);
-    } else if (keyCode === RIGHT_ARROW) {
+    } else if (keyCode === RIGHT_ARROW && snake.xspeed != -1) {
         snake.dir(1, 0);
     }
 }
 
+function drawApple(x, y) {
+    noStroke();
+    push();
+    translate(x, y);
+    fill(204, 55, 51);
+    ellipseMode(CENTER);
+    ellipse(0, 0, 80, 75);
+    stroke(78, 38, 0);
+    strokeWeight(5);
+    line(-5, -50, 0, -25);
+    noStroke();
+    rotate(radians(-30));
+    fill(39, 166, 21);
+    ellipse(7, -33, 15, 25)
+    pop();
+}
+
 function Snake() {
-    this.x = 0;
-    this.y = 0;
+    this.x = 200;
+    this.y = 200;
     this.xspeed = 1;
     this.yspeed = 0;
-    this.total = 0;
+    this.total = 1;
     this.tail = [];
     this.scorePoint = 0;
     let score = document.getElementById('score');
@@ -56,7 +81,22 @@ function Snake() {
         this.yspeed = y;
     }
 
+    this.clear = function () {
+        this.x = 200;
+        this.y = 200;
+        this.xspeed = 1;
+        this.yspeed = 0;
+        this.total = 1;
+        this.tail = [];
+        this.scorePoint = 0;
+        score.innerHTML = `Score : 0`;
+        pickLocation();
+    }
+
     this.update = function () {
+        if (gameOver) {
+            return;
+        }
         if (this.total === this.tail.length) {
             for (let i = 0; i < this.tail.length - 1; i++) {
                 this.tail[i] = this.tail[i + 1];
@@ -69,14 +109,67 @@ function Snake() {
 
         this.x = constrain(this.x, 0, width - scl)
         this.y = constrain(this.y, 0, height - scl)
+
+        if ((this.x <= 0) || (this.x >= width) || (this.y <= 0) || (this.y >= height)) {
+            gameOver = true;
+            console.log('gameover')
+            gameoverElement.classList.add('active');
+        }
+
+        for (let i = 0; i < this.tail.length; i++) {
+            let pos = this.tail[i];
+            let d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                console.log('gameover');
+                gameOver = true;
+                gameoverElement.classList.add('active');
+            }
+
+        }
     }
 
     this.show = function () {
-        fill(255);
+        noStroke();
         for (let i = 0; i < this.tail.length; i++) {
-            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+            fill(0, 255, 0);
+            if (keyCode === UP_ARROW && this.eat) {
+                if (i == this.tail.length - 1) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 5, 5, 0, 0);
+                } else if (i == 0) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 0, 0, 5, 5);
+                } else {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl);
+                }
+            } else if (keyCode === DOWN_ARROW && this.eat) {
+                if (i == this.tail.length - 1) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 0, 0, 5, 5);
+                } else if (i == 0) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 5, 5, 0, 0);
+                } else {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl);
+                }
+            } else if (keyCode === LEFT_ARROW && this.eat) {
+                if (i == this.tail.length - 1) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 5, 0, 0, 5);
+                } else if (i == 0) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 0, 5, 5, 0);
+                } else {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl);
+                }
+            } else if (keyCode === RIGHT_ARROW && this.eat) {
+                if (i == this.tail.length - 1) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 0, 5, 5, 0);
+                } else if (i == 0) {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl, 5, 0, 0, 5);
+                } else {
+                    rect(this.tail[i].x, this.tail[i].y, scl, scl);
+                }
+            } else {
+                rect(this.tail[i].x, this.tail[i].y, scl, scl);
+            }
         }
-        rect(this.x, this.y, scl, scl);
+        fill(25, 67, 167)
+        rect(this.x, this.y, scl, scl, 10);
     }
 
     this.eat = function (pos) {
@@ -91,3 +184,4 @@ function Snake() {
         }
     }
 }
+
